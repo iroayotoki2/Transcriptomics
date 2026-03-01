d0ae972b0f0a1d8c99566a01c8fb7d81af06db15
iroayotoki2 <iroayotoki2@gmail.com>
2026-02-28 21:07:06 -0500

    R script second commit

--- Deseq R script.R
+++ Deseq R script.R
@@ -1,7 +1,17 @@
-library("airway")
 library("DESeq2")
 library("tximport")
 library(ggplot2)
 
 #Load metadata
-sampleTable <- read.csv(file.path(dir,"sample_table.csv"),row.names=1)
+sampleTable <- read.csv("stage_Metadata.csv",row.names=1)
+#Assigning file path and names
+files <- file.path("Gene count tables",paste0(rownames(sampleTable),".fastq.genes.results"))
+names(files) <- rownames(sampleTable)
+#Import 
+#Gene counts already mapped to reference with rsem tx2gene will not be used 
+txi <- tximport(files, type="rsem")
+#Sanity checks
+dim(txi$counts)
+head(txi$counts)
+#Checking that txi data and metadata have same names
+all.equal(colnames(txi$counts), rownames(sampleTable))
